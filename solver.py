a8ddf79f79ab712d6641825096ab48d1c4bd6ec7
logotip <logotip123@yahoo.com>
2019-11-22 12:36:51 +0200

    v 1.1 logotip1992@gmail.com

--- solver.py
+++ solver.py
@@ -1,10 +1,30 @@
 """solve module"""
+import ast
+import operator
+
+binOps = {
+    ast.Add: operator.add,
+    ast.Sub: operator.sub,
+    ast.Mult: operator.mul,
+    ast.Div: operator.floordiv,
+    ast.Mod: operator.mod
+}
 
 
 def solve(expr: str) -> int:
     """solve_"""
-    operations = ["+", "-", "*", "/"]
-    for i in expr:
-        if not i.isdigit() and i not in operations:
-            raise ValueError
-    return eval(expr)
+    node = ast.parse(expr, mode='eval')
+
+    def _eval(node):
+        if isinstance(node, ast.Expression):
+            return _eval(node.body)
+        elif isinstance(node, ast.Str):
+            return node.s
+        elif isinstance(node, ast.Num):
+            return node.n
+        elif isinstance(node, ast.BinOp):
+            return binOps[type(node.op)](_eval(node.left), _eval(node.right))
+        else:
+            raise Exception('Unsupported type {}'.format(node))
+
+    return _eval(node.body)

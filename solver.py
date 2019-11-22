980aa3c6361531af3a3d4a3520022a6baad1ef8c
logotip <logotip123@yahoo.com>
2019-11-22 11:32:18 +0200

    v 1.0 pylint reports an error, but I'm checking string before.

--- solver.py
+++ solver.py
@@ -1,3 +1,10 @@
-def solve(expr: str) -> int:
+"""solve module"""
 
-    return 42
+
+def solve(expr: str) -> int:
+    """solve_"""
+    operations = ["+", "-", "*", "/"]
+    for i in expr:
+        if not i.isdigit() and i not in operations:
+            raise ValueError
+    return eval(expr)

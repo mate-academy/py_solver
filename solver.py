4ccae8c9d16035876327a2e5b5fdabb93d366938
DukhDmytro <cowboybebop4991@gmail.com>
2019-11-24 18:58:34 +0200

    v1

--- solver.py
+++ solver.py
@@ -1,57 +1,58 @@
+"""docstring"""
+
+
 class Stack:
+    """stack"""
     def __init__(self):
         self.items = []
 
     def push(self, item):
+        """push"""
         self.items.append(item)
 
     def pop(self):
+        """pop"""
         return self.items.pop()
 
 
-def solve(expr: str) -> int:
-    char_list = [char for char in expr]
-    print(char_list)
+def solve(expr):
+    """solver"""
+    char_list = []
+    for char in expr:
+        if char.isdigit():
+            char_list.append(int(char))
+        else:
+            char_list.append(char)
 
     stack = Stack()
     i = 0
     while i < len(char_list):
         if char_list[i] == "*":
-            prod = stack.pop() * int(char_list[i+1])
+            prod = stack.pop() * char_list[i+1]
             stack.push(prod)
             i += 2
         elif char_list[i] == "/":
-            div = stack.pop() / int(char_list[i + 1])
-            stack.push(int(div))
+            div = stack.pop() / char_list[i + 1]
+            stack.push(div)
             i += 2
         else:
-            if char_list[i].isdigit():
-                stack.push(int(char_list[i]))
-                i += 1
-            else:
-                stack.push(char_list[i])
-                i +=1
-    char_list = stack.items.copy()
-    print(char_list)
-    stack.items = []
-    i = 0
-    while i < len(char_list):
-        if char_list[i] == "+":
-            print("+")
-            prod = stack.pop() + char_list[i + 1]
-            stack.push(prod)
-            i += 1
-        elif char_list[i] == "-":
-            print("-")
-            div = stack.pop() - char_list[i + 1]
-            stack.push(div)
-            i += 1
-        else:
-            print("els")
             stack.push(char_list[i])
-        i += 1
-    return(stack.items)
+            i += 1
 
+    char_list = stack.items.copy()
+    stack.items = []
+    j = 0
+    while j < len(char_list):
+        if char_list[j] == "+":
+            prod = stack.pop() + char_list[j + 1]
+            stack.push(prod)
+            j += 1
+        elif char_list[j] == "-":
+            div = stack.pop() - char_list[j + 1]
+            stack.push(div)
+            j += 1
+        else:
+            stack.push(char_list[j])
+        j += 1
 
-print(solve("2+2"))
-
+    return stack.pop()

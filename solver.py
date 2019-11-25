2f584d5172ad439a1e13fef242c6e97bee3d71b2
d1makov <makovskyj.danylo@gmail.com>
2019-11-25 17:19:57 +0200

    final

--- solver.py
+++ solver.py
@@ -2,6 +2,9 @@
 docstring
 """
 
+OPERATORS = {'+', '-', '*', '/'}
+PRIORITY = {'+': 1, '-': 1, '*': 2, '/': 2}
+
 
 def solve(expr: str) -> int:
     """
@@ -9,4 +12,42 @@ def solve(expr: str) -> int:
     :param expr:
     :return:
     """
-    return int(eval(expr))
+    return eval_postfix(to_postfix(expr))
+
+
+def to_postfix(expr):
+    """
+
+    :param expr:
+    :return:
+    """
+    stack = []
+    res = ''
+    for char in expr:
+        if char not in OPERATORS:
+            res += char
+        else:
+            while stack and PRIORITY[char] <= PRIORITY[stack[-1]]:
+                res += stack.pop()
+            stack.append(char)
+    while stack:
+        res += stack.pop()
+    return res
+
+
+def eval_postfix(expr):
+    """
+
+    :param expr:
+    :return:
+    """
+    stack = []
+    for char in expr:
+        if char not in OPERATORS:
+            stack.append(int(char))
+        else:
+            b_ch = stack.pop()
+            a_ch = stack.pop()
+            c_ch = {'+': a_ch+b_ch, '-': a_ch-b_ch, '*': a_ch*b_ch, '/': a_ch/b_ch}[char]
+            stack.append(c_ch)
+    return int(stack[-1])
